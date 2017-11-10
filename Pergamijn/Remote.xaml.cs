31b0af671c2a8c8c4302a9c71d65aa8399737a86
Jordi Wijsen <jordiwijsen@outlook.com>
2017-11-10 15:26:29 +0100

    Updated Remote and Youtube content page

--- Pergamijn/Remote.xaml.cs
+++ Pergamijn/Remote.xaml.cs
@@ -16,5 +16,13 @@ namespace Pergamijn
         {
             InitializeComponent();
         }
+
+        void OnTapGestureGoBack(object sender, EventArgs args)
+        {
+            var imageSender = (Image)sender;
+            // Do something
+
+            Navigation.PopModalAsync();
+        }
     }
 }
